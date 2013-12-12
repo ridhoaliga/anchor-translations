9f2562e0caa2cd16e5b11969a3187f28451fc698
kimmonosz <kimmonosz@gmail.com>
2013-12-12 12:15:14 +0700

    Create comments.php

---
+++ id_ID/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+        'comments' => 'Komentar',
+        'nocomments_desc' => 'Tidak ada komentar.',
+        'editing_comment' => 'Editing komentar',
+        'view_comment' => 'Lihat Komentar',
+
+        // form fields
+        'name' => 'Nama',
+        'name_explain' => 'Nama ',
+        'name_missing' => 'Silahkan tambahkan nama anda',
+
+        'email' => 'Email',
+        'email_explain' => 'Email',
+        'email_missing'=> 'Silahkan masukkan email anda yang benar', // frontend message (appears on your site!)
+
+        'text' => 'Comment',
+        'text_explain' => '',
+        'text_missing' => 'Silahkan masukkan komentar anda', // frontend message (appears on your site!)
+
+        'status' => 'Status',
+        'status_explain' => '',
+
+        // messages
+        'created' => 'Komentar anda telah ditambahkan', // frontend message (appears on your site!)
+        'updated' => 'Komentar anda telah diupdate',
+        'deleted' => 'Komentar anda telah dihapus',
+
+        // email notification
+        'notify_subject' => 'Komentar terbaru telah ditambahkan',
+        'nofity_heading' => 'Komentar terbaru telah dipublikasi ke site anda.'
+
+);
